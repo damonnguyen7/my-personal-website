85391c09daed19bc9116702c0056c8f0d308ec5b
Damon <damonnguyen92@gmail.com>
2016-11-18 13:02:52 -0800

    Feat: Add router to middleware and serve static files by adding express.static to middleware

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ var express = require('express');
 var app = express();
 var port = 8080;
 var router = require('./app/routes');
+var path = require('path');
 
-//Add our router to the middleware.
-app.use('/', router);
+app.use('/', router); //Add our router to the middleware.
+app.use(express.static(__dirname + '/public')); //add express.static middleware function to serve static files
+
 
 app.listen(port, function() {
   console.log("Listening on port 8080");
