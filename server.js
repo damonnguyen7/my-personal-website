689fff73311747972b505c71a75a67624880b982
Damon <damonnguyen92@gmail.com>
2016-11-18 12:14:17 -0800

    Refactor: Remove the route logic and move into routes.js

--- server.js
+++ server.js
@@ -1,20 +1,11 @@
 var express = require('express');
 var app = express();
 var port = 8080;
+var router = require('./app/routes');
+
+//Add our router to the middleware.
+app.use('/', router);
 
 app.listen(port, function() {
   console.log("Listening on port 8080");
 });
-
-app.get('/', function(req, res) {
-  res.send('Hello World!');
-});
-
-app.get('/about', function(req, res) {
-  res.send('About page!');
-});
-
-app.get('/contact', function(req, res) {
-  res.send('Contact page!');
-});
-
