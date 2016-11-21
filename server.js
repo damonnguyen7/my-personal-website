5f1b645a1ddfc20eb258b86cd86d8b612072c795
Damon <damonnguyen92@gmail.com>
2016-11-21 13:46:30 -0800

    Refactor: Move ejs related code above router and serve static middleware

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ var path = require('path');
 var expressLayouts = require('express-ejs-layouts');
 
 
+app.set('view engine', 'ejs'); //set up for EJS templating
+app.use(expressLayouts); //override the default response.render() behavior
 
 app.use('/', router); //Add our router to the middleware.
 app.use(express.static(__dirname + '/public')); //add express.static middleware function to serve static files
 
-app.set('view engine', 'ejs'); //set up for EJS templating
-app.use(expressLayouts); //override the default response.render() behavior
 
 app.listen(port, function() {
   console.log("Listening on port 8080");
