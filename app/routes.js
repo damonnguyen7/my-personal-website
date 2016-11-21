ce7c26e39d6adca40aead41f83fdd8941fa37698
Damon <damonnguyen92@gmail.com>
2016-11-21 13:43:56 -0800

    Feat(init): Hard code data about myself and displayed data in about page

--- app/routes.js
+++ app/routes.js
@@ -4,11 +4,24 @@ var path = require('path');
 
 //GET
 router.get('/', function(req, res) {
-  res.render('pages/index'); 
+  res.render('pages/home'); 
 });
 
 router.get('/about', function(req, res) {
-  res.render('pages/about');
+  var damon = {
+    name: 'Damon Nguyen',
+    email: 'damonnguyen92@gmail.com',
+    avatar: 'img/default.png',
+    description: 'I am a full-stack software engineer who has experience working in the frontend and backend. I enjoy building data rich, beautifully designed, scalable, and high performant web application. I enjoy coding because of the never ending learning and solving complex challenges. I am a highly motivated individual who aspires to become a coding ninja! I am looking for an environment where they build impactful products, have a great culture, and an awesome team.'
+  };
+
+  res.render('pages/about', {
+    name: damon.name,
+    email: damon.email,
+    avatar: damon.avatar,
+    description: damon.description
+  });
+
 });
 
 router.get('/contact', function(req, res) {
