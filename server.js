166c98ae4865cd453c467b755706951f97173884
Damon <damonnguyen92@gmail.com>
2016-11-21 10:28:27 -0800

    Feat(init): Require express-layout/add to middleware and set up view engine to EJS

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ var app = express();
 var port = 8080;
 var router = require('./app/routes');
 var path = require('path');
+var expressLayout = require('express-layout');
+
+
+app.use(expressLayout); //override the default response.render() behavior
+app.set('view engine', 'ejs'); //set up for EJS templating
 
 app.use('/', router); //Add our router to the middleware.
 app.use(express.static(__dirname + '/public')); //add express.static middleware function to serve static files
