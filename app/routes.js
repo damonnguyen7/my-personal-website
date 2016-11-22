ea04e4c6a8fea577b8d93fa094576ce5e3466e73
Damon <damonnguyen92@gmail.com>
2016-11-22 13:52:47 -0800

    Refactor: Delete extra var statements, replace handler logic with same logic, but imported from another file for modularity

--- app/routes.js
+++ app/routes.js
@@ -1,59 +1,20 @@
-var express = require('express');
-var router = express.Router();
-var path = require('path');
+var express = require('express'),
+    router = express.Router(),
+    path = require('path'),
+    mainController = require('./controller/main-controller.js');
+
+//Client/Server Handler:
+var landingPage = mainController.landingPage,
+    about = mainController.about,
+    contactGET = mainController.contactGET,
+    contactPOST = mainController.contactPOST;
 
 //GET
-router.get('/', function(req, res) {
-  res.render('pages/home'); 
-});
-
-router.get('/about', function(req, res) {
-  var damon = {
-    name: 'Damon Nguyen',
-    email: 'damonnguyen92@gmail.com',
-    avatar: 'img/default.png',
-    description: 'I am a full-stack software engineer who has experience working in the frontend and backend. I enjoy building data rich, beautifully designed, scalable, and high performant web application. I enjoy coding because of the never ending learning and solving complex challenges. I am a highly motivated individual who aspires to become a coding ninja! I am looking for an environment where they build impactful products, have a great culture, and an awesome team.'
-  };
-
-  var projects = [
-    {
-      name: 'JobPlus',
-      description: 'A web application that gives job searchers insight on their potential employment surroundings by utilizing APIs to deliver relevant information based on their geo-location.',
-      image: 'img/jobPlusDefault.png',
-      link: 'http://jobplus.online/'
-    },
-    {
-      name: 'Spontaneó',
-      description: 'A web application that allows users to generate spontaneous ideas, gifts, and restaurants based off geolocation.',
-      image: 'img/spontaneoDefault.png',
-      link: 'http://spontaneo.herokuapp.com/#/'
-    },
-    {
-      name: 'Pokédex',
-      description: 'A single page web application for fans who wants to learn about the first generation of Pokémon',
-      image: 'img/pokedexDefault.png',
-      link: 'http://pokedex-mvp.herokuapp.com/#/'
-    }
-  ];
-
-  res.render('pages/about', {
-    name: damon.name,
-    email: damon.email,
-    avatar: damon.avatar,
-    description: damon.description,
-    projects: projects
-  });
-
-});
-
-router.get('/contact', function(req, res) {
-  res.render('pages/contact');
-});
+router.get('/', landingPage);
+router.get('/about', about);
+router.get('/contact', contactGET);
 
 //POST
-router.post('/contact', function(req, res) {
-  //log: *name* has contacted you
-  console.log(req.body.name + " has contacted you!");
-});
+router.post('/contact', contactPOST);
 
 module.exports = router;
