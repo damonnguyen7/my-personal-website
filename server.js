e9b9330e5b1fceabc051c70be73c8da765798dfc
Damon <damonnguyen92@gmail.com>
2016-11-22 11:18:39 -0800

    Feat: Load mongoose module, an removed path module

--- server.js
+++ server.js
@@ -2,17 +2,16 @@ var express = require('express');
 var app = express();
 var port = 8080;
 var router = require('./app/routes');
-var path = require('path');
 var expressLayouts = require('express-ejs-layouts');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 app.set('view engine', 'ejs'); //set up for EJS templating
 
 app.use(expressLayouts); //override the default response.render() behavior
-
 app.use(bodyParser.urlencoded({
   extended: true
-})); //parse input url encoded data into objects
+})); //parse input url encoded data into objects and enable query string library
 app.use(bodyParser.json());
 app.use('/', router); //Add our router to the middleware.
 app.use(express.static(__dirname + '/public')); //add express.static middleware function to serve static files
@@ -20,4 +19,4 @@ app.use(express.static(__dirname + '/public')); //add express.static middleware 
 
 app.listen(port, function() {
   console.log("Listening on port 8080");
-});
+}); 
