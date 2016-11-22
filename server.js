803948ec067faee936374f45947e6d522c4dabec
Damon <damonnguyen92@gmail.com>
2016-11-22 10:23:31 -0800

    Feat(init): Require body-parser module, set up body-parser middleware with enabled qs, and parse only json data

--- server.js
+++ server.js
@@ -4,11 +4,16 @@ var port = 8080;
 var router = require('./app/routes');
 var path = require('path');
 var expressLayouts = require('express-ejs-layouts');
-
+var bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs'); //set up for EJS templating
+
 app.use(expressLayouts); //override the default response.render() behavior
 
+app.use(bodyParser.urlencoded({
+  extended: true
+})); //parse input url encoded data into objects
+app.use(bodyParser.json());
 app.use('/', router); //Add our router to the middleware.
 app.use(express.static(__dirname + '/public')); //add express.static middleware function to serve static files
 
