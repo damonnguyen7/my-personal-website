839cb3b4dc3345868a69cf0054f9a5f371def310
Damon <damonnguyen92@gmail.com>
2016-11-22 13:54:34 -0800

    Feat(init): Create and export object full of handler methods

---
+++ app/controller/main-controller.js
@@ -0,0 +1,49 @@
+module.exports = {
+  landingPage: function(req, res) {
+    res.render('pages/home');
+  },
+  about: function(req, res) {
+    var damon = {
+      name: 'Damon Nguyen',
+      email: 'damonnguyen92@gmail.com',
+      avatar: 'img/default.png',
+      description: 'I am a full-stack software engineer who has experience working in the frontend and backend. I enjoy building data rich, beautifully designed, scalable, and high performant web application. I enjoy coding because of the never ending learning and solving complex challenges. I am a highly motivated individual who aspires to become a coding ninja! I am looking for an environment where they build impactful products, have a great culture, and an awesome team.'
+    };
+
+    var projects = [
+      {
+        name: 'JobPlus',
+        description: 'A web application that gives job searchers insight on their potential employment surroundings by utilizing APIs to deliver relevant information based on their geo-location.',
+        image: 'img/jobPlusDefault.png',
+        link: 'http://jobplus.online/'
+      },
+      {
+        name: 'Spontaneó',
+        description: 'A web application that allows users to generate spontaneous ideas, gifts, and restaurants based off geolocation.',
+        image: 'img/spontaneoDefault.png',
+        link: 'http://spontaneo.herokuapp.com/#/'
+      },
+      {
+        name: 'Pokédex',
+        description: 'A single page web application for fans who wants to learn about the first generation of Pokémon',
+        image: 'img/pokedexDefault.png',
+        link: 'http://pokedex-mvp.herokuapp.com/#/'
+      }
+    ];
+
+    res.render('pages/about', {
+      name: damon.name,
+      email: damon.email,
+      avatar: damon.avatar,
+      description: damon.description,
+      projects: projects
+    });
+
+  },
+  contactGET: function(req, res) {
+    res.render('pages/contact');
+  },
+  contactPOST: function(req, res) {
+    console.log(req.body.name + " has contacted you!");
+  }
+}
