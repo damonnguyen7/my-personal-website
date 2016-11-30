9e3e28952bbcef976f893adc6828926859c9868d
Damon <damonnguyen92@gmail.com>
2016-11-29 22:43:17 -0800

    Feat: Add route for allmessageGET to display message from db

--- app/routes.js
+++ app/routes.js
@@ -7,14 +7,17 @@ var express = require('express'),
 var landingPage = mainController.landingPage,
     about = mainController.about,
     contactGET = mainController.contactGET,
+    allmessagesGET = mainController.allmessagesGET,
     contactPOST = mainController.contactPOST;
 
 //GET
 router.get('/', landingPage);
 router.get('/about', about);
 router.get('/contact', contactGET);
+router.get('/allmessages', allmessagesGET);
 
 //POST
 router.post('/contact', contactPOST);
+
 
 module.exports = router;
